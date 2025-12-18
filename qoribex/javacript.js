0c8329045ebcae338686262d04d30ace20b69f93
Esneyders <moralesdurandanderson@gmail.com>
2025-12-18 11:34:44 -0500

    Subiendo el github

--- qoribex/javacript.js
+++ qoribex/javacript.js
@@ -5,11 +5,12 @@ const playlist = [
         "videos/“Cómo documentar un proceso en 10 minutos sin morir en el intento” (1).mp4"
     ];
 
-    let index = 0; 
+    let index = 0; // empieza con el primero
 
     const player = document.getElementById("player");
     const videoSource = document.getElementById("videoSource");
 
+    // Cuando termine un video
     player.addEventListener("ended", () => {
         index++;
 
